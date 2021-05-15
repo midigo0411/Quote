5263c4569d21589dd949ae5020053904a9e0d155
midigo0411 <midigo041@gmail.com>
2021-05-15 15:32:42 +0300

    nesting child component to parent component

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+  @Input()quote: Quote
 
   constructor() { }
 
